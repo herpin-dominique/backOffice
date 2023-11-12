c2ad69d57f3026a2c92d3ae8bc5b1ca417d7bb59
herpin-dominique <117769343+herpin-dominique@users.noreply.github.com>
2023-11-13 00:56:01 +0100

    updateconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      // Ajoutez des alias pour vos chemins ici
+      "@lib": path.resolve(__dirname, "src/lib"),
+      // ...
+    },
+  },
 });
