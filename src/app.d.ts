184f6c5955ebb84c0e646cd14f10a076fde10059
conanjahh <conanjahh@hotmail.com>
2023-12-24 07:24:53 +0100

    setup authentication layer with lucia-auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
+	namespace Lucia {
+		type Auth = import('$lib/server/authentication').Auth;
+		type DatabaseUserAttributes = {
+			username: string;
+			email: string;
+			last_name?: string | null;
+			first_name?: string | null;
+		};
+		type DatabaseSessionAttributes = {};
+	}
+
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: import('$lib/server/authentication').AuthRequest;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
---
+++ src/lib/server/authentication/lucia.ts
@@ -0,0 +1,25 @@
+import { lucia } from 'lucia';
+import { sveltekit } from 'lucia/middleware';
+import { pg, postgres } from '@lucia-auth/adapter-postgresql';
+import { db } from '@vercel/postgres';
+import { AuthenticationTables, postgresSql as sql } from '$lib/server/database';
+import { POSTGRES_HOST } from '$env/static/private';
+
+export const auth = lucia({
+	env: import.meta.env.PROD ? 'PROD' : 'DEV',
+	adapter:
+		POSTGRES_HOST === 'localhost'
+			? postgres(sql, AuthenticationTables)
+			: pg(db, AuthenticationTables),
+	middleware: sveltekit(),
+	getUserAttributes: (databaseUser) => {
+		return {
+			username: databaseUser.username,
+			firstname: databaseUser.first_name,
+			lastname: databaseUser.last_name
+		};
+	}
+});
+
+export type Auth = typeof auth;
+export { type AuthRequest, LuciaError as AuthError } from 'lucia';
