5891981bdac9b458c0740c017a335f4a1c62e235
JConan <conanjahh@hotmail.com>
2023-12-26 17:41:06 +0100

    setup page for root initialization

---
+++ src/lib/server/setup/index.ts
@@ -0,0 +1,43 @@
+import { building } from '$app/environment';
+import { AuthError, auth } from '$lib/server/authentication';
+
+let rootExist = false;
+
+/**
+ * no need to execute this code during built time
+ */
+if (!building) {
+	/**
+	 * Initial Setup Check done once when the server start
+	 */
+	try {
+		await auth.getUser('root');
+		rootExist = true;
+	} catch (e) {
+		if (!(e instanceof AuthError && e.message === 'AUTH_INVALID_USER_ID')) {
+			// show other errors
+			console.error(e);
+		}
+	}
+}
+
+export function isInitialSetup() {
+	return !rootExist;
+}
+
+export async function createRootUser(password: string) {
+	try {
+		await auth.createUser({
+			userId: 'root',
+			key: {
+				providerId: 'system',
+				providerUserId: 'root',
+				password
+			},
+			attributes: {}
+		});
+		rootExist = true;
+	} catch (e) {
+		console.error(e);
+	}
+}
