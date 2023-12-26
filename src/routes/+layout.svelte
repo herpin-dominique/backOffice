c39e44ba50778fb769474f74a19fbc4d1fa28b60
JConan <conanjahh@hotmail.com>
2023-12-26 15:08:35 +0100

    setup flowbite for sveltekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,29 @@
+<script>
+	import { page } from '$app/stores';
+	import '../app.pcss';
+	import { DarkMode } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<img src="/logo.png" class="me-3 h-6 sm:h-9" alt="SND Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>SND Backoffice</span
+		>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Dashboard</NavLi>
+
+		<NavLi href="/tbd">Orders</NavLi>
+		<NavLi href="/tbd">Products</NavLi>
+		<NavLi href="/tbd">Destinations</NavLi>
+
+		<NavLi href="/tbd">Blogs</NavLi>
+		<NavLi href="/tbd">Newsletters</NavLi>
+		<NavLi href="/tbd">Contacts</NavLi>
+	</NavUl>
+	<DarkMode />
+</Navbar>
 <slot />
