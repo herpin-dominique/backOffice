5bf0be873f439ab9454e44198cae1530784d32f7
JConan <conanjahh@hotmail.com>
2023-12-28 04:51:50 +0100

    add empty configuration pages

---
+++ src/routes/configuration/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import { UsersOutline, UserSettingsOutline } from 'flowbite-svelte-icons';
+
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<div class="fluid flex">
+	<Sidebar {activeUrl}>
+		<SidebarWrapper>
+			<SidebarGroup>
+				<SidebarItem label="Users" href="/configuration">
+					<svelte:fragment slot="icon">
+						<UsersOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+			</SidebarGroup>
+			<SidebarGroup>
+				<SidebarItem label="Access rigths" href="/configuration/access-rights">
+					<svelte:fragment slot="icon">
+						<UserSettingsOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+			</SidebarGroup>
+		</SidebarWrapper>
+	</Sidebar>
+
+	<slot />
+</div>
